3f9eb38f95ea361f244df4d7c7b54debd7c30b65
Elin Bohm <elibo867@student.liu.se>
2018-02-02 11:18:15 +0100

    Recreated malfunctioning program

--- For_several.py
+++ For_several.py
@@ -4,14 +4,22 @@
 # In[ ]:
 
 
-import input_mod
+import Get_Densities
 import sys
+
 
 def main():
     density_maps={}
     filenames=sys.argv[1:]
+    #for every file create 11 densitymaps. Generates dict of dicts. Kanske inte är smart?
+    print 'Computing atom densities'
+    counter=1
     for filename in filenames: 
-        density_maps[filename]=input_mod.main(filename)
+        density_maps[filename]=Get_Densities.main(filename)
+        print counter, 'of', len(filenames)
+        counter+=1
+        
+    print 'ready'
         
 main()
 
